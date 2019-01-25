d0545d8bee4949da76134a0b27e85872bb8dc446
zhoujinxin <zhoujinxin@58ganji.com>
2019-01-25 14:42:53 +0800

    company 20190125 14:39 add json

---
+++ src/main/java/json/Entity/Student.java
@@ -0,0 +1,22 @@
+package json.Entity;
+
+public class Student {
+    private String name;
+    private String local;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+}
---
+++ src/main/java/json/Main.java
@@ -0,0 +1,42 @@
+package json;
+
+import json.Entity.Student;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        objectToJson();
+        jsonToObject();
+    }
+
+    private static void jsonToObject() {
+        String objStr = "{\"local\":\"beijing\",\"name\":\"zhoujinxin\"}";
+        JSONObject jsonObject = JSONObject.fromObject(objStr);
+        Student stu = (Student)JSONObject.toBean(jsonObject, Student.class);
+        System.out.println(stu.getLocal());
+        System.out.println(stu.getName());
+
+        String arrayStr = "[{\"local\":\"beijing\",\"name\":\"zhoujinxin\"}]";
+        JSONArray jsonArray = JSONArray.fromObject(arrayStr);
+        for(int i = 0; i < jsonArray.size(); i ++){
+            Object obj = jsonArray.get(i);
+            JSONObject jsonObjectTemp = JSONObject.fromObject(obj);
+            Student student = (Student)jsonObjectTemp.toBean(jsonObjectTemp, Student.class);
+            System.out.println(student.getName());
+            System.out.println(student.getLocal());
+        }
+    }
+
+    public static void objectToJson(){
+        Student stu = new Student();
+        stu.setName("zhoujinxin");
+        stu.setLocal("beijing");
+        JSONObject json = JSONObject.fromObject(stu);
+        System.out.println(json.toString());// {"local":"beijing","name":"zhoujinxin"}
+
+        JSONArray jsonArray = JSONArray.fromObject(stu);
+        System.out.println(jsonArray.toString()); // [{"local":"beijing","name":"zhoujinxin"}]
+    }
+}
