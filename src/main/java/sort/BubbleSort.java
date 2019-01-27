8b492b2273fc7334d9615def4954af1e5dcb9150
zhoujinxin <zhoujinxin@58ganji.com>
2019-01-28 00:49:12 +0800

    company 20190128 0:48 add sort

---
+++ src/main/java/sort/BubbleSort.java
@@ -0,0 +1,36 @@
+package sort;
+
+import java.util.Arrays;
+
+/***
+ * 冒泡排序
+ *  从当前位置向后查找，如果arr[i+1] < arr[i] 则进行交换，每一趟循环都可以找到当前数组的最大值
+ */
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{10, 9, 6, 4, 2,7};
+        System.out.println(Arrays.toString(arr));
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    private static void bubbleSort(int[] arr) {
+        int length = arr.length;
+        for(int i =0; i < length; i ++){
+            for(int j = 0; j < length - i - 1; j ++){
+                if(arr[j] > arr[j+1]){
+//                    int temp = arr[j];
+//                    arr[j] = arr[j+1];
+//                    arr[j+1] = temp;
+                    arr[j] = arr[j] ^ arr[j + 1];
+                    arr[j + 1] = arr[j] ^ arr[j + 1];
+                    arr[j] = arr[j] ^ arr[j + 1];
+                }
+            }
+            System.out.println(" i = " + i + "  " + Arrays.toString(arr));
+        }
+
+    }
+}
