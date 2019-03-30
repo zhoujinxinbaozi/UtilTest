198bee1fce644bec8b42776d3e548b012f5e6863
zhoujinxin <zhoujinxin@58ganji.com>
2019-03-30 12:29:28 +0800

    update Test

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,7 +1,9 @@
 package test;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class Test {
 
@@ -14,6 +16,11 @@ public class Test {
         System.out.println(Calendar.getInstance().getTimeInMillis());// 日期的毫秒显示形式
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
         System.out.println(sdf.format(Calendar.getInstance().getTime()));
-
+        try {
+            Date parse = sdf.parse("20190329");
+            System.out.println(parse);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
