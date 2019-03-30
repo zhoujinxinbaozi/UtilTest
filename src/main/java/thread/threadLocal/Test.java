48159569eb4149f90a99cb525ced684aceb196b5
zhoujinxin <zhoujinxin@58ganji.com>
2019-03-30 19:39:31 +0800

    add thread

---
+++ src/main/java/thread/threadLocal/Test.java
@@ -0,0 +1,27 @@
+package thread.threadLocal;
+
+
+import java.util.Random;
+
+public class Test {
+
+    private static final ThreadLocal<Integer>  threadLocal = new ThreadLocal<>();
+    private static final Random random = new Random();
+    public static void main(String[] args) throws Exception{
+        threadLocal.set(random.nextInt(10));
+        System.out.println(threadLocal.get());
+        System.out.println(Thread.currentThread().getName());
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                threadLocal.set(random.nextInt(11) + 10);
+                System.out.println(threadLocal.get());
+                System.out.println(Thread.currentThread().getName());
+            };
+        };
+        thread.start();
+        thread.join();
+        System.out.println(threadLocal.get());
+        System.out.println(Thread.currentThread().getName());
+    }
+}
