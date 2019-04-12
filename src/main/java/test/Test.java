2f0795380feb0de73747d340432631a45bf8588e
zhoujinxin <zhoujinxin@58ganji.com>
2019-04-12 20:50:23 +0800

    update test.Test

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,11 +1,7 @@
 package test;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test {
 
@@ -15,18 +11,29 @@ public class Test {
 //        System.out.println(Arrays.toString(split));
 //        String os = System.getProperty("os.name");// 操作系统的类型 Windows, linux等
 //        System.out.println(os);
-        System.out.println(Calendar.getInstance().getTimeInMillis());// 日期的毫秒显示形式
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-        System.out.println(sdf.format(Calendar.getInstance().getTime()));
-        try {
-            Date parse = sdf.parse("20190329");
-            System.out.println(parse);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        Map<String, String> mm = new ConcurrentHashMap<>();
-        mm.put("123", null);
+//        System.out.println(Calendar.getInstance().getTimeInMillis());// 日期的毫秒显示形式
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+//        System.out.println(sdf.format(Calendar.getInstance().getTime()));
+//        try {
+//            Date parse = sdf.parse("20190329");
+//            System.out.println(parse);
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//
+//        // 不允许value为null
+//        Map<String, String> mm = new ConcurrentHashMap<>();
+//        mm.put("123", null);
+        List<Integer> list = new ArrayList();
+        list.add(1);
+        list.add(2);
+        pp(list);
+        System.out.println(0%100);
 
     }
+
+    public static void pp(List<?> list){
+        System.out.println(list.toString());
+    }
+
 }
