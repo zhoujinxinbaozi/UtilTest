82db6de265c9be9a5ca1f9fa55e966893a2c13e3
zhoujinxin <zhoujinxin@58ganji.com>
2019-05-19 08:51:09 +0800

    ...

---
+++ src/main/java/log/Main.java
@@ -0,0 +1,11 @@
+package log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        logger.info("你好你好");
+    }
+}
