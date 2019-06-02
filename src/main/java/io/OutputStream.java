90952656fca1a6310e310dffa0a8305db473ed34
zhoujinxin <zhoujinxin@58ganji.com>
2019-06-02 18:17:18 +0800

    up io

--- src/main/java/io/OutputStream.java
+++ src/main/java/io/OutputStream.java
@@ -3,7 +3,6 @@ package io;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
-import java.io.Reader;
 
 public class OutputStream {
     public static void main(String[] args) throws Exception{
